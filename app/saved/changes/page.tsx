ce6c19029dc9be1509156c5b6da6827714208b8a
lyhysyydh <ohanayohaiisrael@gmail.com>
2023-12-06 20:34:19 +0200

    savsaved changes layout

--- app/saved/changes/page.tsx
+++ app/saved/changes/page.tsx
@@ -14,7 +14,7 @@ export default async function Page() {
     
     //get_saved_changes
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2">
+    <div className="grid grid-cols-1 lg:grid-cols-2 lg:px-4 gap-4">
         {data && data.map((change:TChange) => (<ChangeCard change={change} />))}
     </div>
   )
