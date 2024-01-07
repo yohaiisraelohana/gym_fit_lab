3e824bae6d7a1316d05fd78f80e884e6d3d79b43
lyhysyydh <ohanayohaiisrael@gmail.com>
2024-01-07 17:27:31 +0200

    metadata update

--- app/StoreData.tsx
+++ app/StoreData.tsx
@@ -1,6 +1,4 @@
 "use client"
-//! CHECK IF THIS WORKS BECOUSE THERE IS CASE THAT THE USER WILL LOGIN 
-//! AND THE SERVER COMP WILL NOT BE AWARRE TO THE AUTH YET
 import { userStore } from '@/stores/userStore'
 import { createClientComponentClient } from '@supabase/auth-helpers-nextjs';
 
@@ -9,6 +7,7 @@ import React, { useEffect } from 'react'
 export default function StoreData() {
     const {fetchUser , user} = userStore((state)=>state);
     const supabase = createClientComponentClient();
+    
    
 
     const getAuthUser = async () => {
---
+++ app/exercises/page.tsx
@@ -0,0 +1,11 @@
+
+export default async function page() {
+
+  return (
+    <div className=" w-screen h-full flex  ">
+        <h1 className="text-6xl m-auto text-white">
+            GF<span className="text-primary">L</span>
+        </h1>
+    </div>
+  )
+}
